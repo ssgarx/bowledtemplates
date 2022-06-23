3af4ed2fd207f2458b0fa27d0d7bf015c77c7735
ssgarx <sagarchandgadkar74@gmail.com>
2022-06-23 12:34:57 +0530

    aded screen8

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import Screen4 from "./screens/Screen4";
 import Screen5 from "./screens/Screen5";
 import Screen6 from "./screens/Screen6";
 import Screen7 from "./screens/Screen7";
+import Screen8 from "./screens/Screen8";
 function App() {
-  return <Screen7 />;
+  return <Screen8 />;
 }
 
 export default App;
---
+++ src/screens/Screen8.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Uppernav from "../components/Uppernav";
+
+import styles from "./Screen8.module.scss";
+
+import CXPIcon from "../assets/icons/CXPIcon.svg";
+import whatsappIcon from "../assets/icons/whatsappIcon.svg";
+import instagramIcon from "../assets/icons/instagramIcon.svg";
+import twitterIcon from "../assets/icons/twitterIcon.svg";
+import facebookIcon from "../assets/icons/facebookIcon.svg";
+
+function Screen8() {
+  return (
+    <>
+      <Uppernav options={["empty", "bowledIcon", "empty"]} />
+      <div className={styles.box1}>
+        <div className={styles.infoSection}>
+          <p>CricTrumps</p>
+          <p>Wohooo!!!!</p>
+          <p>You won!</p>
+          <p>
+            <span>
+              <img src={CXPIcon} alt="CXPIcon" />
+            </span>
+            1000 CXPs
+          </p>
+        </div>
+        <div className={styles.mediaSection}>
+          <p className={styles.header}>Share with your friends</p>
+          <div>
+            <img src={whatsappIcon} alt="whatsappIcon" />
+            <img src={instagramIcon} alt="instagramIcon" />
+            <img src={twitterIcon} alt="twitterIcon" />
+            <img src={facebookIcon} alt="facebookIcon" />
+          </div>
+          <p className={styles.redirect}>View leaderboard</p>
+        </div>
+        <div className={styles.btnBox}>
+          <button>Play Again</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Screen8;
